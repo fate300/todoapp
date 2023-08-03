198e5126449bacce46704b39311d324d52b7b939
강지행 <jey78v@naver.com>
2023-08-04 01:19:30 +0900

    8월3일 집

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require ('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended : true}));
 
 app.listen(8080,function(){
     console.log('listening on 8080')        
@@ -17,7 +19,28 @@ app.get('/beauty',function(요청, 응답){
     
     });
 
-    app.get('/',function(요청, 응답){
+    app.get('/eng',function(요청, 응답){
+        응답.send('영어공부하는 페이지입니다');
+        
+        });   
+
+app.get('/',function(요청, 응답){
         응답.sendFile(__dirname+'/index.html');
         
+    });
+    
+app.get('/write',function(요청, 응답){
+        응답.sendFile(__dirname+'/write.html');
+        
         });
+// 어떤 사람이 /add 경로로 POST 요청을 하면 ???를 해주세요
+//app.post('경로',콜백함수) 
+//요청에 입력받은 내용 저장됨
+//꺼내서 쓰려면 body.parser라는 library가 필요함  
+
+app.post('/add',function(요청,응답){
+    응답.send('전송완료')
+    console.log(요청.body.title);
+    console.log(요청.body.date);
+    //DB에 저장해주세요. 
+});
