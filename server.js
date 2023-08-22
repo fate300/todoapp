f1c78c37489c000e9f495a65fe09418f5e3b3224
강지행 <jey78v@naver.com>
2023-08-23 00:46:30 +0900

    8월22일 집

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended : true}));
 const MongoClient = require('mongodb').MongoClient; 
 app.set('view engine', 'ejs');
-app.use('public',express.static('public'));
-
+app.use('/public',express.static('public'));
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'))
 
 var db;
 
@@ -31,7 +32,22 @@ MongoClient.connect('mongodb+srv://fate300000:JEy33783808@cluster0.mqbkngy.mongo
 }) 
 
 
+app.get('/write',function(요청,응답){
+    응답.render('write.ejs')
+})
 
+app.get('/',function(요청,응답){
+    응답.render('index.ejs')
+})
+
+
+app.get('/edit/:id',function(요청,응답){
+    db.collection('post').findOne({_id:parseInt(요청.params.id)}, function(에러,결과){
+        console.log(결과)
+        응답.render('edit.ejs',{post:결과})
+    })
+   
+})
 
 
 
@@ -59,10 +75,13 @@ app.get('/',function(요청, 응답){
         
     });
     
-app.get('/write',function(요청, 응답){
-        응답.sendFile(__dirname+'/write.html');
+// app.get('/write',function(요청, 응답){
+//         응답.sendFile(__dirname+'/write.html');
         
-        });
+//         });
+
+
+
 // 어떤 사람이 /add 경로로 POST 요청을 하면 ???를 해주세요
 //app.post('경로',콜백함수) 
 //요청에 입력받은 내용 저장됨
