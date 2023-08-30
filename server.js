f96a2e514e76551a76fffb73f544e3d09acfafb6
강지행 <jey78v@naver.com>
2023-08-31 02:01:47 +0900

    8월30일 집

--- server.js
+++ server.js
@@ -79,10 +79,6 @@ app.get('/beauty',function(요청, 응답){
         
         });   
 
-app.get('/',function(요청, 응답){
-        응답.sendFile(__dirname+'/index.html');
-        
-    });
     
 // app.get('/write',function(요청, 응답){
 //         응답.sendFile(__dirname+'/write.html');
@@ -139,22 +135,18 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const session = require ('express-session');
 
-
 app.use(session({secret:'비밀코드', resave: true, saveUninitialized: false}));
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-app.get('/login',function(요청,응답){
-    응답.render('login.ejs')
-
+app.get('/login', function(요청,응답){
+    응답.render('login.ejs');
 });
 
-app.post('/login',passs.authenticate('local',{
-    failureRediret:'/fail'
-
+app.post('/login', passport.authenticate('local',{
+    failureRedirect:'/fail'
 }), function(요청,응답){
- 응답.redirect('/')
+    응답.redirect('/');
 });
 
 
@@ -177,6 +169,13 @@ passport.use(new LocalStrategy({
     })
   }));
 
+passport.serializeUser(function(user,done){
+done(null,user.id)
+
+}); 
+passport.deserializeUser(function(아이디,done){
+done(null,{})
+});
 
 
 
