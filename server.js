a15c8c2ad6c81633116b3f56060af14a7a315519
강지행 <jey78v@naver.com>
2023-10-06 23:57:01 +0900

    10월7일 집

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const MongoClient = require('mongodb').MongoClient;
 app.set('view engine', 'ejs');
 app.use('/public',express.static('public'));
 const methodOverride = require('method-override')
-const{ ObjectId } = require('mongodb');
 app.use(methodOverride('_method'))
 
  var db;
@@ -242,7 +241,7 @@ app.post('/chatroom', function(요청, 응답){
 
     var 저장할거 = {
       title : '무슨무슨채팅방',
-      member : [ObjectId(요청.body.당한사람id), 요청.user._id],
+      member : [요청.body.당한사람id, 요청.user._id],
       date : new Date()
     }
 
@@ -276,6 +275,23 @@ app.post('/message',로그인했니,function(요청,응답){
     })
     });
     
+
+app.get('/message/:id',로그인했니,function(요청,응답){
+응답.writeHead(200,{
+"Connection":"keep-alive",
+"Content-Type":"text/event-stream",
+"Cache-Control":"no-cache",
+});
+
+db.collection('message').find({parent:요청.params.id}).toArray().then((결과)=>
+
+{
+응답.write('event:test\n');
+응답.write('data:'+JSON.stringify(결과)+'\n\n');
+})
+});
+
+
 
 
 app.post('/add',function(요청,응답){
